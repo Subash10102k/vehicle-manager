cd0fde79e44111a7811beed6fb531e90a284693d
subash <subashraja3366@gmail.com>
2023-03-07 20:01:05 +0530

    created RaptorEngine class and methods

---
+++ RaptorEngine.java
@@ -0,0 +1,8 @@
+public class RaptorEngine{
+		public static void start() {
+			System.out.println("Raptor Engine ingnition starting");
+		}
+		public static void stop() {
+			System.out.println("Raptor Engine stops");
+		}
+	}
