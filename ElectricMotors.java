b5cc06234513782651289d12b19f5b1e52467340
subash <subashraja3366@gmail.com>
2023-03-07 19:33:18 +0530

    created stop method in electric motor class

--- ElectricMotors.java
+++ ElectricMotors.java
@@ -2,4 +2,7 @@ public class ElectricMotor{
 	public static void start() {
 		System.out.println("Electric Motor starting");
 	}
+	public static void stop() {
+		System.out.println("Electric Motor stops");
+	}
 }
