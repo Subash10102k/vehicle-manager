ba26e75bfdab51ccb923ae63c87ea3670bd5bae7
subash <subashraja3366@gmail.com>
2023-03-07 20:03:11 +0530

    intiated method in main class

--- Vehicle.java
+++ Vehicle.java
@@ -4,10 +4,16 @@ public class Vehicle{
 		 PetrolEngine pe = new PetrolEngine();
 		 DieselEngine de = new DieselEngine();
 		 ElectricMotor ee = new ElectricMotor();
+		 RaptorEngine re = new RaptorEngine();
 		pe.start();
 		pe.stops();
 		
 		de.start();
 		de.stops();
+		
+		re.start();
+		re.stops();
+		
+		
 	}
 }
