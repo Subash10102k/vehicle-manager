ace4285292e07034d48b91146c4eab82fc2c3212
subash <subashraja3366@gmail.com>
2023-03-10 19:42:43 +0530

    added power function in electricmotors

--- ElectricMotors.java
+++ ElectricMotors.java
@@ -5,4 +5,9 @@ public class ElectricMotor{
 	public static void stop() {
 		System.out.println("Electric Motor stops");
 	}
+	public static void power() {
+		System.out.println("started powering motors");
+	}
+	
+	
 }
